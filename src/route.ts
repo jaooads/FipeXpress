cf5554e9263b610b7476940ec10f5f09183ec423
jaooads <joaoarthur24062006@gmail.com>
2025-09-30 10:08:55 -0300

    Estuturação de pastas, intalação Sequelize, TypeScript

---
+++ src/route.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { getMarcas, getModelos, getValor } from "./controllers/consultaFipeController.ts";
+
+const router = Router();
+
+// Retorna todas as marcas de carros
+router.get("/marcas", getMarcas);
+
+// Retorna todos os modelos de uma marca
+router.get("/modelos/:codigoMarca", getModelos);
+
+// Retorna o valor de um veículo pelo código FIPE
+router.get("/valor/:codigoFipe", getValor);
+
+export default router;
